a42112508cec606633c445c70c2367697736fe92
sergeev_ms <sergeevms.33@gmail.com>
2024-05-02 12:50:34 +0300

    Initial

---
+++ src/test/java/ru/sms/configserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sms.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
